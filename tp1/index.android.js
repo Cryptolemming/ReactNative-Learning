b52f5fa8bb269b3a2200980059a611a35fcb503b
cryptolemming <aliayoub22@gmail.com>
2015-12-28 16:36:30 -0500

    PanResponder demo for handling touch events to move a circle

--- tp1/index.android.js
+++ tp1/index.android.js
@@ -6,38 +6,16 @@
 var React = require('react-native');
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
 } = React;
 var Button = require('./Button');
+var PanResponderDemo = require('./PanResponderDemo');
 
 var Application = React.createClass({
   render: function() {
     return (
-        <Button/>
+        <PanResponderDemo/>
     );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
 AppRegistry.registerComponent('tp1', () => Application);
