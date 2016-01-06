738fe942835523521ece2dbd59f06605c45a4fbb
cryptolemming <aliayoub22@gmail.com>
2016-01-05 19:42:25 -0500

    added flex styling and called the component from index

--- Experimental/Flexbox.js
+++ Experimental/Flexbox.js
@@ -21,15 +21,21 @@ var FlexTest = React.createClass({
 
 var styles = StyleSheet.create({
 	parent: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'flex-start',
 		backgroundColor: '#F5FCFF',
 		borderColor: '#0099AA',
 		borderWidth: 5,
 		marginTop: 30,
 	},
 	child: {
+		flex: 1,
 		borderColor: '#AA0099',
 		borderWidth: 2,
 		textAlign: 'center',
 		fontSize: 24,
 	}
 });
+
+module.exports = FlexTest;
--- Experimental/index.android.js
+++ Experimental/index.android.js
@@ -11,11 +11,12 @@ var Button = require('./Button');
 var PanResponderDemo = require('./PanResponderDemo');
 var BookList = require('./ListViewDemo/BookList');
 var CrossPlatform = require('./CrossPlatformDemo/CrossPlatform');
+var FlexTest = require('./Flexbox');
 
 var Application = React.createClass({
   render: function() {
     return (
-        <CrossPlatform />
+        <FlexTest />
     );
   }
 });
