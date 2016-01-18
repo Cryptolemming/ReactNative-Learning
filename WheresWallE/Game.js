c90cfa5059904008f0f7f88c696b43fc3bf23614
cryptolemming <aliayoub22@gmail.com>
2016-01-17 19:34:53 -0500

    created initial component shells

---
+++ WheresWallE/Game.js
@@ -0,0 +1,28 @@
+var React = require('react-native');
+var {
+	Text,
+	View,
+	StyleSheet
+} = React;
+
+'use strict';
+
+var Card = React.createClass({
+
+});
+
+var newGameButton = React.createClass({
+
+});
+
+var winModal = React.createClass({
+
+});
+
+var Board = React.createClass({
+
+});
+
+var Game = React.createClass({
+
+});
