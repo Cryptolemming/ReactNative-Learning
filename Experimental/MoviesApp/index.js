b5a0345167de9fc4ed05261bfde8e8e14fbcd252
cryptolemming <aliayoub22@gmail.com>
2016-02-03 20:13:24 -0500

    addedd fetchData for Rotten Tomatoes API form Facebook as well as state change and render functions

--- Experimental/MoviesApp/index.js
+++ Experimental/MoviesApp/index.js
@@ -11,11 +11,50 @@ var {
 var MOCKED_MOVIES_DATA = [
 	{title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 var MoviesApp = React.createClass({
+	getInitialState() {
+		return {
+			movies: null
+		};
+	},
+
+	componentDidMount() {
+		this.fetchData();
+	},
+
+	fetchData() {
+		fetch(REQUEST_URL)
+			.then((response) => response.json())
+			.then((responseData) => {
+				this.setState({
+					movies: responseData.movies,
+				});
+			})
+			.done();
+	},
 
 	render() {
-		var movie = MOCKED_MOVIES_DATA[0];
+		if(!this.state.movies) {
+			return this.renderLoadingView();
+		}
+
+		var movie = this.state.movies[0];
+		return this.renderMovie(movie);
+	},
+
+	renderLoadingView() {
+		return (
+			<View style={styles.container}>
+				<Text>
+					Loading Movies...
+				</Text>
+			</View>
+		);
+	},
+
+	renderMovie(movie) {
 		return (
 			<View style={styles.container}>
 				<Image 
