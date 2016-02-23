367edbdc14385eb9b68c07151d787e148668bbfd
cryptolemming <aliayoub22@gmail.com>
2016-02-23 15:08:43 -0500

    minor styling changes

--- WheresWallEMobile/index.android.js
+++ WheresWallEMobile/index.android.js
@@ -11,7 +11,7 @@ import React, {
 import Game from './Game.js';
 
 var backgroundImage = 'https://dl.dropboxusercontent.com/s/quaxsr8vn3hdi9e/background.jpg?dl=0';
-const CARDS = ['k3xkgdci3h9mlnfa/walle.jpg?dl=0', 'k3xkgdci3h9mlnf/walle.jpg?dl=0', '1ll4rd0q28y7is8/eve.jpg?dl=0', '7sbiokkeq2hnaze/john.jpg?dl=0', '93ltebnju2vd5ns/captain2.jpg?dl=0', 'uho6nbflui260ca/mary.jpg?dl=0'];
+const CARDS = ['k3xkgdci3h9mlnf/walle.jpg?dl=0', 'epmgt0g9on02unj/walle2.jpg?dl=0', '1ll4rd0q28y7is8/eve.jpg?dl=0', '7sbiokkeq2hnaze/john.jpg?dl=0', '93ltebnju2vd5ns/captain2.jpg?dl=0', 'uho6nbflui260ca/mary.jpg?dl=0'];
 
 class WheresWallEMobile extends Component {
   render() {
